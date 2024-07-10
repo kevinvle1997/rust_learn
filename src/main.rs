adfc23831173869ebabc54ab2d9dcf169f3c3dc8
Kevin Le <32471289+kevinvle1997@users.noreply.github.com>
2024-07-09 23:47:31 -0500

    new sprite

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ struct AnimationIndices {
     last: usize,
 }
 
+#[derive(Component, PartialEq, Eq, Copy)]
+enum PlayerState {
+    Running,
+    Neutral,
+    Jumping,
+}
+
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
@@ -38,8 +45,8 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let texture = asset_server.load("sprites/running.png");
-    let layout = TextureAtlasLayout::from_grid(UVec2::splat(16), 6, 1, None, None);
+    let texture = asset_server.load("sprites/person.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(16), 6, 2, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
     // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 0, last: 5 };
@@ -56,30 +63,51 @@ fn setup(
         },
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        PlayerState::Neutral,
     ));
 }
 
 fn movement(
     time: Res<Time>,
-    mut sprite_position: Query<(&mut Transform, &mut AnimationTimer, &mut AnimationIndices)>,
+    mut sprite_position: Query<(&mut Transform, &mut AnimationTimer, &mut AnimationIndices, &mut PlayerState)>,
     keyboard_input: Res<ButtonInput<KeyCode>>
 ) {
-    for (mut transform, mut animation_timer, mut animation_indices) in &mut sprite_position {
-        println!("Animation indices: {:?}", animation_indices);
+    for (mut transform, mut animation_timer, mut animation_indices, mut player_state) in &mut sprite_position {
         if keyboard_input.pressed(KeyCode::KeyA) {
+            animation_indices.first = 0;
+            animation_indices.last = 5;
+            *player_state = PlayerState::Running;
             transform.translation.x -= 150. * time.delta_seconds();
             transform.scale.x = -6.0; // Flip the sprite horizontally
             animation_timer.0.unpause();
         } if keyboard_input.pressed(KeyCode::KeyD) {
+            animation_indices.first = 0;
+            animation_indices.last = 5;
+            *player_state = PlayerState::Running;
             transform.translation.x += 150. * time.delta_seconds();
             animation_timer.0.unpause();
             transform.scale.x = 6.0; // Flip the sprite horizontally
         } if keyboard_input.pressed(KeyCode::Space) {
-            animation_indices.first = 0;
-            animation_indices.last = 2;
+            jump(time, &mut transform, &mut animation_timer, &mut animation_indices, player_state);
         } if !keyboard_input.pressed(KeyCode::KeyA) && !keyboard_input.pressed(KeyCode::KeyD) || (keyboard_input.pressed(KeyCode::KeyD) && keyboard_input.pressed(KeyCode::KeyA)){
+            *player_state = PlayerState::Neutral;
+        }
+
+        if *player_state == PlayerState::Neutral {
             animation_timer.0.pause();
         }
     }
 }
 
+fn jump(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut AnimationTimer, &mut AnimationIndices, &mut PlayerState)>,
+) {
+    for (mut transform, mut animation_timer, mut animation_indices, mut player_state) in query.iter_mut() {
+        transform.translation.y += 200. * time.delta_seconds();
+        animation_indices.first = 6;
+        animation_indices.last = 11;
+        *player_state = PlayerState::Jumping;
+        animation_timer.0.reset();
+        animation_timer.0.unpause();    }
+}
